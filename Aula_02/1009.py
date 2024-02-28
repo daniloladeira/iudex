f5d6d19212e18521282faa23e256c46a9530ac73
Danilo <142433003+daniloladeira@users.noreply.github.com>
2024-02-28 14:30:07 -0300

    Create 1009.py

---
+++ Aula_02/1009.py
@@ -0,0 +1,9 @@
+nome = input()
+salario_fixo = float(input())
+vendas_do_mes = float(input())
+
+total = salario_fixo + (vendas_do_mes-0.15)
+
+total_float = float(total_float)
+
+print(f'TOTAL = R$ ", {total_float:.2f}')
