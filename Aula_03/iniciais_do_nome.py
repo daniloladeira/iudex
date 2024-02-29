0d7e3e649d9b0a43497a4d97157391dc9d1af8ab
Danilo <142433003+daniloladeira@users.noreply.github.com>
2024-02-29 15:51:58 -0300

    Update iniciais_do_nome.py

--- Aula_03/iniciais_do_nome.py
+++ Aula_03/iniciais_do_nome.py
@@ -1,3 +1,5 @@
+- SOLUÇÃO_01:
+
 def iniciais(nome):
     palavras = nome.split()
     iniciais = ""
@@ -6,4 +8,14 @@ def iniciais(nome):
     return(iniciais)
 
 nome = input("Digite seu nome: \n")
+print(iniciais(nome))
+
+- SOLUÇÃO_02:
+
+def iniciais(nome):
+    palavras = nome.split()
+    iniciais = [palavra[0] for palavra in palavras]
+    return (iniciais)
+
+nome = input("Digite seu nome: ")
 print(iniciais(nome))
